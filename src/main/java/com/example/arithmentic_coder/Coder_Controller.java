9aabe20aedc7bad86e5f2398c18674f3ab6d745a
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-12-07 14:48:45 -0800

    Finished a round draft of building arithmetic coding algorithm. Intakes A B inputs with 0.5%, outputs the upper and lower bounds of arithmetic coding

--- src/main/java/com/example/arithmentic_coder/Coder_Controller.java
+++ src/main/java/com/example/arithmentic_coder/Coder_Controller.java
@@ -1,23 +1,55 @@
 package com.example.arithmentic_coder;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 
-import java.io.File;
+import java.io.*;
 
 public class Coder_Controller {
 
     File textFile;
 
     @FXML
-    protected void onOpenFileClicked() {
+    private Label inputText;
+
+    @FXML
+    protected void onOpenFileClicked() throws IOException {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select your wav file");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", ".txt"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        fileChooser.setInitialDirectory(new File("C:\\Users\\User\\Downloads\\project-3-samples\\MM\\Q1\\"));
+
         textFile = fileChooser.showOpenDialog(null);
 
         if(textFile != null){
 
+            BufferedReader reader = new BufferedReader(new FileReader(textFile));
+            int n = Integer.parseInt(reader.readLine());
+            System.out.println(n);
+
+            String outputStr = "";
+
+            String line = reader.readLine();
+            while(line!= null){
+                double low = 0;
+                double mid = 0.5;
+                double high = 1;
+
+                for(int i=0; i<line.length(); i++){
+                    if(line.charAt(i) == 'A') {
+                        high = mid;
+                        mid = low + ((high-low)*0.5);
+                    } else{
+                        low = mid;
+                        mid = low + ((high-low)*0.5);
+                    }
+                }
+                outputStr = outputStr + "Input: " + line + "   ||   Low: "+ low + "   ||   High: " + high + "\n";
+                line = reader.readLine();
+            }
+            inputText.setText(outputStr);
         }
     }
 }
